773c591307159a1ed2ed8a138b2bc2c7309ebbcc
Handshake <100866254+Handshaking@users.noreply.github.com>
2022-12-07 17:10:07 -0700

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -1,34 +1,43 @@
 const ethers = require('ethers');
 const Web3 = require("web3");
-const minSweep = '0.005';
+const minSweep = '0.004';
+
+
+
+const chosenRpc = 'Your EVM RPC'
 const compromisedPrivkey = 'Your Compromised Privkey';
+const compromisedPubkey  = 'Your Compromised Pubkey');
+const destinationPubkey = 'Your Destination Address');
+
+
 
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms));  }
-
-
-
 async function main() {
-	const web3 = new Web3('https://bsc-dataseed.binance.org/');
-	const compromisedPubkey  = web3.utils.toChecksumAddress('Your Compromised Pubkey');
-	const destinationPubkey = web3.utils.toChecksumAddress('Your Destination Address');
+	const compromisedPubkeyChk  = web3.utils.toChecksumAddress(compromisedPrivkey);
+	const destinationPubkeyChk = web3.utils.toChecksumAddress(compromisedPubkey);
+	const web3 = new Web3(chosenRPC);
 	const gasGwei = await web3.utils.toWei('30', 'gwei');
-	const ethMin = await web3.utils.toWei("0.004", 'ether');
+	const ethMin = await web3.utils.toWei(minSweep, 'ether');
 	var counter = 0;
 	var done = 0;
 	var errors = 0;
-
-
-
 	while (true) {
 	counter++;
-    var balance = await web3.eth.getBalance(compromisedPubkey)
+    var balance = await web3.eth.getBalance(compromisedPubkeyChk)
 	console.log(balance)
     if (Number(balance) > Number(ethMin)) {
     	try {
-	      let nonce = await web3.eth.getTransactionCount(compromisedPubkey);
-	      let trnsAmount = Number(balance) - gasGwei * 40000;
-	      let txPrice = { 'chainId': 1, 'nonce':Number(nonce) + 1, 'to':destinationPubkey, 'value': trnsAmount, 'gas':41000, 'gasPrice':Number(gasGwei) };
-	      let signedTx = await web3.eth.accounts.signTransaction(txPrice, compromisedPrivkey); // private key
+	      let nonce = await web3.eth.getTransactionCount(compromisedPubkeyChk);
+	      let trnsAmount = Number(balance) - (gasGwei * 40000);
+	      let txPrice = { 
+		 	'chainId': 1,
+		      	'nonce': Number(nonce) + 1,
+		      	'to': destinationPubkeyChk,
+		        'value': trnsAmount,
+		        'gas': 41000,
+		        'gasPrice': Number(gasGwei)};
+		
+	      let signedTx = await web3.eth.accounts.signTransaction(txPrice, compromisedPrivkeyChk); // private key
 	      let txHash = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
 	      let amntSent = await web3.utils.fromWei(String(trnsAmnt), 'ether');
 	      done++;
@@ -37,10 +46,11 @@ async function main() {
 	    } catch (e) {
 	    	console.log(e);
 	    	errors++;
-			await sleep(500);
+		await sleep(500);
 	    }
     } else {
     	let view = await web3.utils.fromWei(String(balance), 'ether');
+	await sleep(2000);
     }
 	}
 }
